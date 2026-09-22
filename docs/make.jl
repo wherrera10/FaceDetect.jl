a81494060faf0bd0db88f4e2505dedcb0dd57fd0
Bill <wherrera10@users.noreply.github.com>
2026-09-22 08:51:54 -1000

    Create make.jl

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using Documenter, FaceDetect
+
+DocMeta.setdocmeta!(FaceDetect, :DocTestSetup, :(using FaceDetect); recursive=true)
+
+makedocs(;
+    modules=[FaceDetect],
+    authors="William Herrera",
+    sitename="FaceDetect.jl Documentation",
+    repo=Documenter.Remotes.GitHub("wherrera10", "FaceDetect.jl"),
+    format=Documenter.HTML(;
+        canonical="https://wherrera10.github.io/FaceDetect.jl",
+        edit_link="master",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+        # Add future pages here, e.g., "API Reference" => "api.md"
+    ],
+)
+
+deploydocs(;
+    repo="github.com/wherrera10/FaceDetect.jl.git",
+    devbranch="master",
+)
