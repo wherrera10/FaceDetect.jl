6f4810d1fde263bbe00a6eeb210cba689ad9add5
Bill <wherrera10@users.noreply.github.com>
2026-09-22 10:47:31 -1000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -9,7 +9,7 @@ makedocs(;
     repo=Documenter.Remotes.GitHub("wherrera10", "FaceDetect.jl"),
     format=Documenter.HTML(;
         canonical="https://wherrera10.github.io/FaceDetect.jl",
-        edit_link="master",
+        edit_link="gh-pages",
         assets=String[],
     ),
     pages=[
